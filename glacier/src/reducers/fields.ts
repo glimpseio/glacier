10283a632345669254e5b8834b7a336829e48752
Colin Ferris <colin@colinferris.com>
2016-11-21 15:00:35 -0500

    Started working on remove filter.

--- glacier/src/reducers/fields.ts
+++ glacier/src/reducers/fields.ts
@@ -1,6 +1,12 @@
 import {AnyDataSource, FieldState} from "../model";
 import {AllActions} from "../actions";
 
+// function filter(state: FieldState, toRemove: string[]) {
+//     Object.keys(state).map(k => k).filter(function(v) {
+//         console.log(k)
+//     });
+// }
+
 export function fields(state: FieldState | undefined, action: AllActions) {
      if (!state) return {};
      if (action.error) {
@@ -12,6 +18,8 @@ export function fields(state: FieldState | undefined, action: AllActions) {
              return { ...state, uuids: [...action.payload.uuids] };
          }
          case "REMOVE_FIELDS": {
+             //TODO
+             //filter(state, action.payload.uuids);
              return {};
          }
          default: return {};
